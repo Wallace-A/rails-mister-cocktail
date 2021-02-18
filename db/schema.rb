828ce280fb64a064eff6f1e7adf6f2a8be50f2a0
Andrew Wallace <andrew.wallace.english@gmail.com>
2021-02-18 19:32:00 +0900

    now we dance

--- db/schema.rb
+++ db/schema.rb
@@ -23,11 +23,11 @@ ActiveRecord::Schema.define(version: 2021_02_18_031645) do
 
   create_table "doses", force: :cascade do |t|
     t.string "description"
-    t.bigint "cocktails_id", null: false
+    t.bigint "cocktail_id", null: false
     t.bigint "ingredient_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["cocktails_id"], name: "index_doses_on_cocktails_id"
+    t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
     t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
   end
 
@@ -37,6 +37,6 @@ ActiveRecord::Schema.define(version: 2021_02_18_031645) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "doses", "cocktails", column: "cocktails_id"
+  add_foreign_key "doses", "cocktails"
   add_foreign_key "doses", "ingredients"
 end
